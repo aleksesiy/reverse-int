db5bba369a69ea9c1a6590300aea72ab63744177
aleksesiy <lexa.svsk@gmail.com>
2024-09-28 14:11:18 +0700

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+        let snm = n.toString();
+        let result = '';
+        let i = snm.length - 1;
+        while (i >= 0) {
+          result = result + snm[i];
+          i--;
+        }
+        return parseInt(result);
+      
+      
 }
